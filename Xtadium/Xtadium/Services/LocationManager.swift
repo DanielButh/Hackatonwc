f8116d391c7a6de76f4ddeb2293e36c081a2a26f
MacBook 6 <macbook6@MacBook-6.local>
2025-10-21 18:00:16 -0600

    final

---
+++ Xtadium/Xtadium/Services/LocationManager.swift
@@ -0,0 +1,18 @@
+//
+//  LocationManager.swift
+//  Xtadium
+//
+//  Created by MacBook 6 on 21/10/25.
+//
+
+import SwiftUI
+
+struct LocationManager: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationManager()
+}
---
+++ Xtadium/Xtadium/ViewModels/RutaCalculadora.swift
@@ -0,0 +1,18 @@
+//
+//  RutaCalculadora.swift
+//  Xtadium
+//
+//  Created by MacBook 6 on 21/10/25.
+//
+
+import SwiftUI
+
+struct RutaCalculadora: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RutaCalculadora()
+}
